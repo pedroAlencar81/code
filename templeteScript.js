7c0f47c79ee08f015adc52675c4f4ecd7a9e5a75
pedroAlencar81 <pedro.alencar@addweb.com.br>
2023-12-15 10:56:31 -0300

    AddScripte

---
+++ templeteScript.js
@@ -0,0 +1,10 @@
+const nA = 4;
+const nB = 5;
+
+const frases = `
+       A adição de: ${nA} + ${nB} = ${nA + nB}
+    A subtração de: ${nA} - ${nB} = ${nA - nB}
+A multiplicação de: ${nA} x ${nB} = ${nA * nB}
+      A divisão de: ${nA} / ${nB} = ${nA / nB}
+`
+console.log(frases);
